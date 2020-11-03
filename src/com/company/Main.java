ff8332f84d0ec915219b68b4e8d8062fa5bbf3fb
TheJakey <jakub.marincic@gmail.com>
2020-11-03 09:49:43 +0100

    Voters

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,15 +13,10 @@ public class Main {
     private static final Current myCurrent = new Current();
 
     private static final ReentrantLock mutex = new ReentrantLock();
-    private static final Condition collectorsCondition = mutex.newCondition();
-    private static final Condition huntersCondition = mutex.newCondition();
-    private static int collectorsCount = 0;
-    private static int huntersCount = 0;
+    private static final ReentrantLock voteBoxMutex = new ReentrantLock();
+    private static final ReentrantLock votedMutex = new ReentrantLock();
 
-    private static final ReentrantLock hunterGiftMutex = new ReentrantLock();
-    private static final ReentrantLock collectorGiftMutex = new ReentrantLock();
-    private static int hunterGiftsCount = 0;
-    private static int collectorGiftsCount = 0;
+    private static int votedCount = 0;
 
     private static boolean stop = false;
 
@@ -29,33 +24,28 @@ public class Main {
     public static void main(String[] args) {
         int i;
 
-        List<Thread> hunters = new ArrayList<>();
-        List<Thread> collectors = new ArrayList<>();
+        List<Thread> voters = new ArrayList<>();
 
-        for (i = 0; i < 6; i++) hunters.add(new HunterThread(i));
-        for (i = 0; i < 12; i++) collectors.add(new CollectorThread(i));
+        for (i = 0; i < 100; i++) voters.add(new VoterThread(i));
 
-        startThreads(hunters);
-        startThreads(collectors);
+        DisplayThread displayThread = new DisplayThread();
+        displayThread.start();
 
-        sleep(30000);
-        stop = true;
-
-        // end the misery of threads and finish them all
-        mutex.lock();
-        collectorsCondition.signalAll();
-        huntersCondition.signalAll();
-        mutex.unlock();
+        for (Thread voter : voters) {
+            voter.start();
+            sleep(1000);
+        }
 
         try {
-            for (Thread thread : hunters) thread.join();
-            for (Thread thread : collectors) thread.join();
+            for (Thread thread : voters) thread.join();
+
+            // end also display thread
+            stop = true;
+            displayThread.join();
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        System.out.println("Hunter gited: " + hunterGiftsCount);
-        System.out.println("Collector gifted: " + collectorGiftsCount);
     }
 
     public static void sleep(int miliseconds) {
@@ -70,120 +60,80 @@ public class Main {
             thread.start();
         }
     }
-    private static void waitForSignal(Condition queue) {
+    private static void awaitForSignal(Condition queue) {
         try {
             queue.await();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
+    private static void waitForNotify(Current queue) {
+        try {
+            queue.wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
     static class Current {
-        public final List<Thread> queue = new ArrayList<>();
-        public int inside = 0;
+        public int currentlyVoting = 0;
     }
 
-    static class HunterThread extends Thread {
+    static class DisplayThread extends Thread {
+        @Override
+        public void run() {
+            while (!stop) {
+                votedMutex.lock();
+                System.out.println("Voted: " + votedCount);
+                votedMutex.unlock();
+
+                Main.sleep(5000);
+            }
+        }
+    }
+
+    static class VoterThread extends Thread {
 
         private final Integer myId;
 
-        public HunterThread(Integer id) {
+        public VoterThread(Integer id) {
             this.myId = id;
         }
 
-        public void doAction() {
-            Main.sleep(6000);
-        }
-
-        public void giveGift() {
+        public void vote() {
             Main.sleep(2000);
-            hunterGiftMutex.lock();
-            hunterGiftsCount++;
-            hunterGiftMutex.unlock();
         }
 
-        @Override
-        public void run() {
-            // till not stopped
-            while (!stop) {
-                doAction();
-
-
-                mutex.lock();
-                while ((collectorsCount != 0 || huntersCount == 2) && !stop) {
-                    waitForSignal(collectorsCondition);
-                }
-                if (stop) {
-                    mutex.unlock();
-                    break;
-                }
-                huntersCount++;
-                System.out.println("Hunter entering \nHunterID: " + myId + "\nNumber of hunters in with me: " + huntersCount);
-                System.out.println();
-                mutex.unlock();
-
-
-                giveGift();
-
-
-                mutex.lock();
-                huntersCount--;
-
-                huntersCondition.signalAll();
-                mutex.unlock();
-            }
-        }
-
-    }
-
-    static class CollectorThread extends Thread {
-
-        private final Integer myId;
-
-        public CollectorThread(Integer id) {
-            this.myId = id;
-        }
-
-        public void doAction() {
-            Main.sleep(4000);
-        }
-
-        public void giveGift() {
+        public void submitVote() {
             Main.sleep(1000);
-            collectorGiftMutex.lock();
-            collectorGiftsCount++;
-            collectorGiftMutex.unlock();
+            votedMutex.lock();
+            votedCount++;
+            votedMutex.unlock();
         }
 
         @Override
         public void run() {
-            // till not stopped
-            while (!stop) {
-                doAction();
+//            voteBoxMutex.lock();
+//            while (myCurrent.currentlyVoting)
+            synchronized (myCurrent) {
+                while (myCurrent.currentlyVoting == 3)
+                    waitForNotify(myCurrent);
 
-
-                mutex.lock();
-                while ((huntersCount != 0 || collectorsCount == 4) && !stop) {
-                    waitForSignal(huntersCondition);
-                }
-                if (stop) {
-                    mutex.unlock();
-                    break;
-                }
-                collectorsCount++;
-                System.out.println("Collector entering \nCollectorID: " + myId + "\nNumber of Collector in with me: " + collectorsCount);
-                System.out.println();
-                mutex.unlock();
-
-                giveGift();
-
-
-                mutex.lock();
-                collectorsCount--;
-
-                collectorsCondition.signalAll();
-                mutex.unlock();
+                myCurrent.currentlyVoting++;
+//                System.out.println("MyID: " + myId + "   -- Currently voting: " + myCurrent.currentlyVoting);
             }
+
+            vote();
+
+            synchronized (myCurrent) {
+//                System.out.println("MyID: " + myId + "   -- Voted. Submiting vote.");
+                myCurrent.currentlyVoting--;
+            }
+
+            mutex.lock();
+            submitVote();
+//            System.out.println("MyID: " + myId + "   -- I left");
+            mutex.unlock();
         }
 
     }
